2d18b543d33abdc98d268a871f5219e5c444810c
Guido Flohr <guido@imperia.net>
2009-09-21 21:12:57 +0300

    Can now connect and disconnect with stubs for DNS.

---
+++ src/gibbon-resolver.h
@@ -0,0 +1,54 @@
+/* This file is part of Gibbon
+ * Copyright (C) 2009 Guido Flohr
+ * 
+ * Gibbon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Gibbon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Gibbon; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _GIBBON_RESOLVER_H
+#define _GIBBON_RESOLVER_H
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+#define GIBBON_TYPE_RESOLVER             (gibbon_resolver_get_type ())
+#define GIBBON_RESOLVER(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), GIBBON_TYPE_RESOLVER, GibbonResolver))
+#define GIBBON_RESOLVER_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), GIBBON_TYPE_RESOLVER, GibbonResolverClass))
+#define GIBBON_IS_RESOLVER(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GIBBON_TYPE_RESOLVER))
+#define GIBBON_IS_RESOLVER_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), GIBBON_TYPE_RESOLVER))
+#define GIBBON_RESOLVER_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), GIBBON_TYPE_RESOLVER, GibbonResolverClass))
+
+typedef struct _GibbonResolverClass   GibbonResolverClass;
+typedef struct _GibbonResolver        GibbonResolver;
+typedef struct _GibbonResolverPrivate GibbonResolverPrivate;
+
+struct _GibbonResolverClass
+{
+        GObjectClass parent_class;
+};
+
+GType gibbon_resolver_get_type (void) G_GNUC_CONST;
+
+struct _GibbonResolver
+{
+        GObject parent_instance;
+        GibbonResolverPrivate *priv;
+};
+
+GibbonResolver *gibbon_resolver_new (const gchar *hostname);
+gint gibbon_resolver_resolve (GibbonResolver *self);
+
+G_END_DECLS
+
+#endif
---
+++ src/gibbon.h
@@ -0,0 +1,29 @@
+/* This file is part of Gibbon
+ * Copyright (C) 2009 Guido Flohr
+ * 
+ * Gibbon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Gibbon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Gibbon; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _GIBBON_H
+#define _GIBBON_H
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include "gibbon-connection.h"
+
+extern GibbonConnection *connection;
+
+#endif
