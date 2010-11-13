3928bac2fe0288395a890f4e1a5afcd5d71ad4b0
Guido Flohr <guido@imperia.net>
2010-11-13 08:15:25 +0200

    Round-trip test for mininimal.sgf.

---
+++ libgsgf/tests/test-write-minimal.c
@@ -0,0 +1,49 @@
+/*
+ * This file is part of Gibbon, a graphical frontend to the First Internet 
+ * Backgammon Server FIBS.
+ * Copyright (C) 2009 Guido Flohr, http://guido-flohr.net/.
+ *
+ * Gibbon is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gibbon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gibbon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdio.h>
+
+#include <glib/gi18n.h>
+
+#include "test.h"
+
+char *filename = "minimal.sgf";
+
+int 
+test_collection(GSGFCollection *collection, GError *error)
+{
+        GOutputStream *out = g_memory_output_stream_new(NULL, 0, NULL, NULL);
+        gssize written = gsgf_collection_write_stream(collection, out, NULL, &error);
+        gchar *expect = "(;)";
+        gchar *got;
+
+        if (error) return expect_error(error, NULL);
+
+        got = (gchar*) g_memory_output_stream_get_data(out);
+        if (strcmp(got, expect)) {
+                fprintf(stderr, "Expected '(;'), got '%s'.\n", got);
+                return -1;
+        }
+
+        return 0;
+}
