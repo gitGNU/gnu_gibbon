20b436ebfd1cb4463f4c947b50453a3d44c0ba3c
Guido Flohr <guido@imperia.net>
2010-12-27 23:01:29 +0200

    Test for KO move property.

---
+++ libgsgf/tests/test-move-properties.c
@@ -0,0 +1,88 @@
+/*
+ * This file is part of Gibbon, a graphical frontend to the First Internet 
+ * Backgammon Server FIBS.
+ * Copyright (C) 2009-2010 Guido Flohr, http://guido-flohr.net/.
+ *
+ * Gibbon is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gibbon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gibbon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <glib/gi18n.h>
+#include <stdio.h>
+
+#include "test.h"
+
+char *filename = "move-properties.sgf";
+
+static gboolean test_prop_KO(const GSGFNode *node);
+
+int 
+test_collection(GSGFCollection *collection, GError *error)
+{
+        GList *game_trees;
+        GSGFGameTree *game_tree;
+        GList *nodes;
+        GList *item;
+        GSGFNode *node;
+
+        if (error) {
+                fprintf(stderr, "%s: %s\n", filename, error->message);
+                return -1;
+        }
+
+        game_trees = gsgf_collection_get_game_trees(collection);
+        if (!game_trees) {
+                fprintf(stderr, "No game trees found.\n");
+                return -1;
+        }
+        game_tree = GSGF_GAME_TREE(game_trees->data);
+
+        nodes = gsgf_game_tree_get_nodes(game_tree);
+        if (!nodes) {
+                fprintf(stderr, "No nodes found.\n");
+                return -1;
+        }
+
+        item = g_list_nth_data(nodes, 1);
+        if (!item) {
+                fprintf(stderr, "Property #1 not found.\n");
+                return -1;
+        }
+        node = GSGF_NODE(item);
+        if (!test_prop_KO(node))
+                return -1;
+
+        return expect_error(error, NULL);
+}
+
+static gboolean
+test_prop_KO(const GSGFNode *node)
+{
+        const GSGFCookedValue *cooked_value = gsgf_node_get_property_cooked(node, "KO");
+
+        if (!cooked_value) {
+                fprintf(stderr, "No property 'KO'!\n");
+                return FALSE;
+        }
+
+        if (!GSGF_IS_MOVE(cooked_value)) {
+                fprintf(stderr, "Property 'KO' is not a GSGFMove!\n");
+                return FALSE;
+        }
+
+        return TRUE;
+}
