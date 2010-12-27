5ada224b8b485480b1a336ce5a4c97209019be67
Guido Flohr <guido@imperia.net>
2010-12-27 08:57:05 +0200

    Prototypes for gsgs_game_tree_get_nodes/children.

--- libgsgf/gsgf-game-tree.h
+++ libgsgf/gsgf-game-tree.h
@@ -62,6 +62,8 @@ struct _GSGFNode;
 GSGFGameTree *gsgf_game_tree_add_child(GSGFGameTree *self);
 GSGFGameTree *gsgf_game_tree_get_parent(const GSGFGameTree *self);
 struct _GSGFNode *gsgf_game_tree_add_node(GSGFGameTree *self);
+GList *gsgf_game_tree_get_nodes(const GSGFGameTree *self);
+GList *gsgf_game_tree_get_children(const GSGFGameTree *self);
 
 G_END_DECLS
 
