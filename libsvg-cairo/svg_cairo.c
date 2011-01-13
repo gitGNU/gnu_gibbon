56dda4decfc605cd08935ca0c4355e1050993e39
Guido Flohr <guido@imperia.net>
2011-01-13 08:52:29 +0100

    Updated copyright date.

--- libsvg-cairo/svg_cairo.c
+++ libsvg-cairo/svg_cairo.c
@@ -1,7 +1,7 @@
 /* libsvg-cairo - Render SVG documents using the cairo library
  *
  * Copyright (C) 2002 University of Southern California
- * Copyright (C) 2009-2010 Guido Flohr
+ * Copyright (C) 2009-2011 Guido Flohr
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
