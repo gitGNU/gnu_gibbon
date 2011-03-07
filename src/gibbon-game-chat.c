ba323883d01adfbaf7dae463d14e1b8254dbf70d
Guido Flohr <guido@imperia.net>
2011-03-07 11:44:29 +0200

    Put files under GPL.

--- src/gibbon-game-chat.c
+++ src/gibbon-game-chat.c
@@ -1,20 +1,20 @@
 /*
- * This file is part of gibbon.
- * Gibbon is a Gtk+ frontend for the First Internet Backgammon Server FIBS.
+ * This file is part of Gibbon, a graphical frontend to the First Internet
+ * Backgammon Server FIBS.
  * Copyright (C) 2009-2011 Guido Flohr, http://guido-flohr.net/.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
+ * Gibbon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * Gibbon is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with gibbon; if not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with Gibbon.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
