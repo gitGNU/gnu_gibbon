a165d747f3819cf3d5589db6e5352830397c5b99
Guido Flohr <guido@imperia.net>
2011-03-09 20:02:25 +0200

    Test for creating complete matches with libgsgf.

---
+++ src/test-basic-match.c
@@ -0,0 +1,80 @@
+/*
+ * This file is part of Gibbon, a graphical frontend to the First Internet 
+ * Backgammon Server FIBS.
+ * Copyright (C) 2009-2011 Guido Flohr, http://guido-flohr.net/.
+ *
+ * Gibbon is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Gibbon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gibbon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <glib.h>
+
+#include <gibbon-match.h>
+
+static GibbonMatch *fill_match (void);
+static gboolean serialize_match (const GibbonMatch *match);
+
+int
+main(int argc, char *argv[])
+{
+	int status = 0;
+	GibbonMatch *match;
+
+        g_type_init ();
+
+        match = fill_match ();
+        if (!match)
+                return -1;
+
+        if (!serialize_match (match))
+                status = -1;
+
+        g_object_unref (match);
+
+        return status;
+}
+
+static GibbonMatch *
+fill_match (void)
+{
+        GibbonMatch *match = gibbon_match_new ();
+
+        return match;
+}
+
+static gboolean
+serialize_match (const GibbonMatch *match)
+{
+        const GSGFCollection *collection = gibbon_match_get_collection (match);
+        GOutputStream *out = g_memory_output_stream_new (NULL, 0,
+                                                         g_realloc, g_free);
+        GError *error = NULL;
+        gsize written;
+
+        if (!gsgf_component_write_stream (GSGF_COMPONENT (collection), out,
+                                          &written, NULL, &error)) {
+                g_printerr ("Writing basic match failed: %s.\n",
+                            error->message);
+                return FALSE;
+        }
+
+        g_printerr ("%s", g_memory_output_stream_get_data  (out));
+
+        g_object_unref (out);
+
+        return TRUE;
+}
