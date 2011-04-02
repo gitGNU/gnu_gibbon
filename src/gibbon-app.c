27a3b8d32e3cb51befc6676ec84f8a2ad2198609
Guido Flohr <guido@imperia.net>
2011-04-02 18:28:17 +0300

    Match actual board size.

--- src/gibbon-app.c
+++ src/gibbon-app.c
@@ -353,7 +353,7 @@ gibbon_app_init_board (GibbonApp *self, const gchar *board_filename)
         /* FIXME! This should occupy reasonable space by default!  Do
          * not hardcode the values.
          */
-        gtk_widget_set_size_request (GTK_WIDGET (board), 500, 400);
+        gtk_widget_set_size_request (GTK_WIDGET (board), 490, 490);
 
         dummy = gibbon_app_find_object (self, "dummy-drawingarea",
                                         GTK_TYPE_DRAWING_AREA);
