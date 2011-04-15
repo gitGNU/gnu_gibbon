51b7d4edef83b406abac0c9d45b570d218174b88
Guido Flohr <guido@imperia.net>
2011-04-15 18:41:26 +0200

    Test for one blocked move after doubles.

--- src/test-moves.c
+++ src/test-moves.c
@@ -32,6 +32,7 @@ static gboolean expect_move (const GibbonMove *expect,
 
 static gboolean test_white_simple_doubles (void);
 static gboolean test_black_simple_doubles (void);
+static gboolean test_white_3moves_doubles (void);
 
 int
 main(int argc, char *argv[])
@@ -43,6 +44,8 @@ main(int argc, char *argv[])
         if (!test_white_simple_doubles ())
                 status = -1;
         if (!test_black_simple_doubles ())
+                status = -1;
+        if (!test_white_3moves_doubles ())
                 status = -1;
 
         return status;
@@ -152,7 +155,6 @@ test_white_simple_doubles ()
 
         return retval;
 }
-
 
 static gboolean
 test_black_simple_doubles ()
@@ -306,6 +308,66 @@ static gboolean expect_move (const GibbonMove *expect,
         }
 
         g_free (got);
+
+        return retval;
+}
+
+
+static gboolean
+test_white_3moves_doubles ()
+{
+        GibbonPosition *before = gibbon_position_new ();
+        GibbonPosition *after;
+        GibbonMove *move;
+        GibbonMove *expect;
+        gint i;
+        gboolean retval = TRUE;
+
+        expect = g_alloca (sizeof expect->number
+                           + 4 * sizeof *expect->movements
+                           + sizeof expect->status);
+
+        before->match_length = 1;
+        before->dice[0] = 3;
+        before->dice[1] = 3;
+
+        for (i = 0; i < 24; ++i)
+                before->points[i] = 0;
+
+        /*
+         *  +12-13-14-15-16-17-------18-19-20-21-22-23-+ negative: black or X
+         *  |   -2    +1       |   |    +1 -2       +1 |
+         * v|                  |BAR|                   |
+         *  |+1    -2          |   | -2                |
+         *  +11-10--9--8--7--6--------5--4--3--2--1--0-+ positive: white or O
+         */
+        before->points[23] = +1;
+        before->points[20] = -2;
+        before->points[19] = +1;
+        before->points[15] = +1;
+        before->points[13] = -2;
+        before->points[11] = +1;
+        before->points[9] = -2;
+        before->points[5] = -2;
+
+        /* White moves each of his movable checkers 2 pips.  */
+        after = gibbon_position_copy (before);
+        after->points[19] = 0;
+        after->points[16] = 1;
+        after->points[15] = 0;
+        after->points[12] = 1;
+        after->points[11] = 0;
+        after->points[8] = 1;
+
+        expect->number = 3;
+        expect->status = GIBBON_MOVE_LEGAL;
+        move = gibbon_position_check_move (before, after,
+                                           GIBBON_POSITION_SIDE_WHITE);
+
+        if (!expect_move (expect, move, "White moved 3 checkers after 33"))
+                retval = FALSE;
+
+        gibbon_position_free (after);
 
         return retval;
 }
