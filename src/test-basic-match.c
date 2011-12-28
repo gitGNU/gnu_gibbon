2b800096eb184119e9fcf6489758e505a637105a
Guido Flohr <guido@imperia.net>
2011-12-29 00:23:50 +0200

    Alternating sides for rolls

--- src/test-basic-match.c
+++ src/test-basic-match.c
@@ -87,7 +87,7 @@ fill_match (void)
         gibbon_game_add_action (game, GIBBON_POSITION_SIDE_WHITE, action);
 
         action = GIBBON_GAME_ACTION (gibbon_roll_new (5, 2));
-        gibbon_game_add_action (game, GIBBON_POSITION_SIDE_WHITE, action);
+        gibbon_game_add_action (game, GIBBON_POSITION_SIDE_BLACK, action);
 
         action = GIBBON_GAME_ACTION (gibbon_move_newv (5, 2, 12, 17, 1, 3, -1));
         gibbon_game_add_action (game, GIBBON_POSITION_SIDE_BLACK, action);
@@ -100,7 +100,7 @@ fill_match (void)
         gibbon_game_add_action (game, GIBBON_POSITION_SIDE_WHITE, action);
 
         action = GIBBON_GAME_ACTION (gibbon_roll_new (4, 5));
-        gibbon_game_add_action (game, GIBBON_POSITION_SIDE_WHITE, action);
+        gibbon_game_add_action (game, GIBBON_POSITION_SIDE_BLACK, action);
 
         action = GIBBON_GAME_ACTION (gibbon_move_newv (4, 5, 0, 4, -1));
         gibbon_game_add_action (game, GIBBON_POSITION_SIDE_BLACK, action);
