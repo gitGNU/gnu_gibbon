26150763b9402af28efbcda5dfa6d6fbeceda2f8
Guido Flohr <guido@imperia.net>
2012-01-14 17:43:27 +0200

    GibbonMatchWriter must have initial position

--- src/gibbon-game.c
+++ src/gibbon-game.c
@@ -528,6 +528,14 @@ gibbon_game_is_crawford (const GibbonGame *self)
 }
 
 const GibbonPosition *
+gibbon_game_get_initial_position (const GibbonGame *self)
+{
+        g_return_val_if_fail (GIBBON_IS_GAME (self), NULL);
+
+        return self->priv->initial_position;
+}
+
+const GibbonPosition *
 gibbon_game_get_nth_position (const GibbonGame *self, gint n)
 {
         const GibbonGameSnapshot *snapshot;
