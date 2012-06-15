f8abae50fb1eefca0231f0f82c8d74ac201b2152
Guido Flohr <guido@imperia.net>
2012-06-15 23:48:18 +0300

    Test match completion with a drop

--- src/test_match_completion.sh
+++ src/test_match_completion.sh
@@ -3,6 +3,10 @@
 dir=${srcdir:='.'}
 
 ./test_match_complete $dir/maxfill-white-incomplete.gmd \
-        $dir/maxfill-white-complete.gmd
+        $dir/maxfill-white-complete.gmd || exit 1
 ./test_match_complete $dir/maxfill-black-incomplete.gmd \
-        $dir/maxfill-black-complete.gmd
+        $dir/maxfill-black-complete.gmd || exit 1
+./test_match_complete $dir/drop-white-incomplete.gmd \
+        $dir/drop-white-complete.gmd || exit 1
+./test_match_complete $dir/drop-black-incomplete.gmd \
+        $dir/drop-black-complete.gmd || exit 1
