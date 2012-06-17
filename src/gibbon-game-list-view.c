19731d27b5285eae9d6d99e06b84231ef597b640
Guido Flohr <guido@imperia.net>
2012-06-17 14:19:46 +0300

    Skeleton for game list combo view

---
+++ src/gibbon-game-list-view.c
@@ -0,0 +1,85 @@
+/*
+ * This file is part of gibbon.
+ * Gibbon is a Gtk+ frontend for the First Internet Backgammon Server FIBS.
+ * Copyright (C) 2009-2012 Guido Flohr, http://guido-flohr.net/.
+ *
+ * gibbon is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gibbon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gibbon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * SECTION:gibbon-game-list-view
+ * @short_description: Game list combo.
+ *
+ * Since: 0.2.0
+ *
+ * The combo with the list of games in the moves tab.
+ */
+
+#include <glib.h>
+#include <glib/gi18n.h>
+
+#include "gibbon-game-list-view.h"
+
+typedef struct _GibbonGameListViewPrivate GibbonGameListViewPrivate;
+struct _GibbonGameListViewPrivate {
+        const GibbonMatchList *list;
+};
+
+#define GIBBON_GAME_LIST_VIEW_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), \
+        GIBBON_TYPE_GAME_LIST_VIEW, GibbonGameListViewPrivate))
+
+G_DEFINE_TYPE (GibbonGameListView, gibbon_game_list_view, G_TYPE_OBJECT)
+
+static void 
+gibbon_game_list_view_init (GibbonGameListView *self)
+{
+        self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
+                GIBBON_TYPE_GAME_LIST_VIEW, GibbonGameListViewPrivate);
+
+        self->priv->list = NULL;
+}
+
+static void
+gibbon_game_list_view_finalize (GObject *object)
+{
+        G_OBJECT_CLASS (gibbon_game_list_view_parent_class)->finalize(object);
+}
+
+static void
+gibbon_game_list_view_class_init (GibbonGameListViewClass *klass)
+{
+        GObjectClass *object_class = G_OBJECT_CLASS (klass);
+        
+        g_type_class_add_private (klass, sizeof (GibbonGameListViewPrivate));
+
+        object_class->finalize = gibbon_game_list_view_finalize;
+}
+
+/**
+ * gibbon_game_list_view_new:
+ * @list: The #GibbonMatchList .
+ *
+ * Creates a new #GibbonGameListView.
+ *
+ * Returns: The newly created #GibbonGameListView or %NULL in case of failure.
+ */
+GibbonGameListView *
+gibbon_game_list_view_new (const GibbonMatchList *list)
+{
+        GibbonGameListView *self = g_object_new (GIBBON_TYPE_GAME_LIST_VIEW, NULL);
+
+        self->priv->list = FALSE;
+
+        return self;
+}
