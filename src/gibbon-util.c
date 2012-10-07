db29a4036e0391d8c32c6163d8115a6b71930acb
Guido Flohr <guido@imperia.net>
2012-10-07 14:11:26 +0300

    Typo

--- src/gibbon-util.c
+++ src/gibbon-util.c
@@ -267,7 +267,7 @@ gibbon_chareq (const char *str1, const char *str2)
         if (!str1 || !str1[0] || str1[1]
             || !str2 || !str2[0] || str2[1])
                 return FALSE;
-        return str1[1] ==  str2[1];
+        return str1[0] ==  str2[0];
 }
 
 gdouble
