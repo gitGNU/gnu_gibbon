f272c9945b4da01419e25851e607fda50cbfd379
Guido Flohr <guido@imperia.net>
2012-10-26 13:12:11 +0300

    write match skeleton

--- src/gibbon-match-tracker.c
+++ src/gibbon-match-tracker.c
@@ -35,10 +35,14 @@
 #include "gibbon-match.h"
 #include "gibbon-app.h"
 #include "gibbon-archive.h"
+#include "gibbon-gmd-writer.h"
 
 typedef struct _GibbonMatchTrackerPrivate GibbonMatchTrackerPrivate;
 struct _GibbonMatchTrackerPrivate {
         GibbonMatch *match;
+        gchar *outname;
+        GibbonGMDWriter *writer;
+        GOutputStream *out;
 };
 
 #define GIBBON_MATCH_TRACKER_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), \
@@ -53,15 +57,31 @@ gibbon_match_tracker_init (GibbonMatchTracker *self)
                 GIBBON_TYPE_MATCH_TRACKER, GibbonMatchTrackerPrivate);
 
         self->priv->match = NULL;
+        self->priv->outname = NULL;
+        self->priv->writer = NULL;
 }
 
 static void
 gibbon_match_tracker_finalize (GObject *object)
 {
         GibbonMatchTracker *self = GIBBON_MATCH_TRACKER (object);
+        GError *error = NULL;
+
+        if (self->priv->out && !g_output_stream_close (self->priv->out,
+                                                       NULL, &error)) {
+                gibbon_app_fatal_error (app, _("Write Error"),
+                                        _("Error writing to `%s': %s!\n"),
+                                        self->priv->outname,
+                                        error->message);
+        }
 
         if (self->priv->match)
                 g_object_unref (self->priv->match);
+
+        g_free (self->priv->outname);
+
+        if (self->priv->writer)
+                g_object_unref (self->priv->writer);
 
         G_OBJECT_CLASS (gibbon_match_tracker_parent_class)->finalize(object);
 }
@@ -92,18 +112,44 @@ GibbonMatchTracker *
 gibbon_match_tracker_new (const gchar *player1, const gchar *player2,
                           gsize length, gboolean resume)
 {
-        GibbonMatchTracker *self = g_object_new (GIBBON_TYPE_MATCH_TRACKER, NULL);
+        GibbonMatchTracker *self = g_object_new (GIBBON_TYPE_MATCH_TRACKER,
+                                                 NULL);
         GibbonArchive *archive = gibbon_app_get_archive (app);
-        gchar *saved_name;
+        GFile *file;
+        GFileOutputStream *fout;
+        GError *error = NULL;
+        GibbonMatchWriter *writer;
 
-        saved_name = gibbon_archive_get_saved_name (archive, player1, player2);
-        g_free (saved_name);
+        self->priv->outname = gibbon_archive_get_saved_name (archive, player1,
+                                                             player2);
+
+        file = g_file_new_for_path (self->priv->outname);
+        fout = g_file_replace (file, NULL, FALSE, G_FILE_COPY_NONE,
+                               NULL, &error);
+        g_object_unref (file);
+        if (!fout) {
+                gibbon_app_fatal_error (app, _("Write Error"),
+                                        _("Error writing to `%s': %s!\n"),
+                                        self->priv->outname,
+                                        error->message);
+        }
+        self->priv->out = G_OUTPUT_STREAM (fout);
 
         /*
          * We always assume that the Crawford rule applies for fixed-length
          * matches.  We will find out whether this is correct or not later.
          */
         self->priv->match = gibbon_match_new (player1, player2, length, length);
+        self->priv->writer = gibbon_gmd_writer_new ();
+        writer = GIBBON_MATCH_WRITER (self->priv->writer);
+
+        if (!gibbon_match_writer_write_stream (writer, self->priv->out,
+                                               self->priv->match, &error)) {
+                gibbon_app_fatal_error (app, _("Write Error"),
+                                        _("Error writing to `%s': %s!\n"),
+                                        self->priv->outname,
+                                        error->message);
+        }
 
         return self;
 }
