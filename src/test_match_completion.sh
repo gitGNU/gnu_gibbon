c22d3aafe07f6b082b49886a279a1646e0614af3
Guido Flohr <guido@imperia.net>
2012-10-27 20:10:46 +0300

    tests for match completion in initial match phase

--- src/test_match_completion.sh
+++ src/test_match_completion.sh
@@ -1,24 +1,39 @@
 #! /bin/sh
 
 dir=${srcdir:='.'}
+exit_code=0
 
 ./test_match_complete $dir/maxfill-white-incomplete.gmd \
-        $dir/maxfill-white-complete.gmd || exit 1
+        $dir/maxfill-white-complete.gmd || exit_code=1
 ./test_match_complete $dir/maxfill-black-incomplete.gmd \
-        $dir/maxfill-black-complete.gmd || exit 1
+        $dir/maxfill-black-complete.gmd || exit_code=1
 ./test_match_complete $dir/drop-white-incomplete.gmd \
-        $dir/drop-white-complete.gmd || exit 1
+        $dir/drop-white-complete.gmd || exit_code=1
 ./test_match_complete $dir/drop-black-incomplete.gmd \
-        $dir/drop-black-complete.gmd || exit 1
+        $dir/drop-black-complete.gmd || exit_code=1
 ./test_match_complete $dir/roll-white-incomplete.gmd \
-        $dir/roll-white-complete.gmd || exit 1
+        $dir/roll-white-complete.gmd || exit_code=1
 ./test_match_complete $dir/roll-black-incomplete.gmd \
-        $dir/roll-black-complete.gmd || exit 1
+        $dir/roll-black-complete.gmd || exit_code=1
 ./test_match_complete $dir/reject-white-incomplete.gmd \
-        $dir/reject-white-complete.gmd || exit 1
+        $dir/reject-white-complete.gmd || exit_code=1
 ./test_match_complete $dir/reject-black-incomplete.gmd \
-        $dir/reject-black-complete.gmd || exit 1
+        $dir/reject-black-complete.gmd || exit_code=1
 ./test_match_complete $dir/reject2-white-incomplete.gmd \
-        $dir/reject2-white-complete.gmd || exit 1
+        $dir/reject2-white-complete.gmd || exit_code=1
 ./test_match_complete $dir/reject2-black-incomplete.gmd \
-        $dir/reject2-black-complete.gmd || exit 1
+        $dir/reject2-black-complete.gmd || exit_code=1
+./test_match_complete $dir/initial-with-game.gmd \
+        $dir/opening-white-complete.gmd || exit_code=1
+./test_match_complete $dir/initial-with-game.gmd \
+        $dir/opening-black-complete.gmd || exit_code=1
+./test_match_complete $dir/initial.gmd \
+        $dir/opening-white-complete.gmd || exit_code=1
+./test_match_complete $dir/initial.gmd \
+        $dir/opening-black-complete.gmd || exit_code=1
+./test_match_complete $dir/initial-with-double.gmd \
+        $dir/opening-white-double-complete.gmd || exit_code=1
+./test_match_complete $dir/initial-with-double.gmd \
+        $dir/opening-black-double-complete.gmd || exit_code=1
+
+exit $exit_code
