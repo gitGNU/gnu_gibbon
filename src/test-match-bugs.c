ac48c55aa30b905fb43daa4831bb847ac989354c
Guido Flohr <guido@imperia.net>
2013-01-09 23:17:10 +0200

    new match continuation test

--- src/test-match-bugs.c
+++ src/test-match-bugs.c
@@ -31,7 +31,7 @@
 
 gboolean
 test_bug01 ()
-{	
+{
         GibbonMatch *match;
         GibbonGame *game;
         GibbonPosition *from;
@@ -79,12 +79,68 @@ test_bug01 ()
         return TRUE;
 }
 
+gboolean
+test_bug02 ()
+{
+        GibbonMatch *match;
+        GibbonGame *game;
+        GibbonPosition *from;
+        GibbonPosition *to;
+        GSList *result = NULL;
+        GibbonRoll *roll;
+
+        match = gibbon_match_new ("gnubg_grandmaster", "gnubg_advanced",
+                                  11, TRUE);
+        g_return_val_if_fail (gibbon_match_add_game (match, NULL), -1);
+        game = gibbon_match_get_current_game (match);
+        from = gibbon_game_get_initial_position_editable (game);
+
+        from->turn = GIBBON_POSITION_SIDE_WHITE;
+        memset (from->points, 0, sizeof from->points);
+        from->dice[0] = from->dice[1] = +5;
+        from->cube = 2;
+        from->may_double[0] = FALSE;
+        from->may_double[1] = TRUE;
+        from->points[2] = +2;
+        from->points[4] = +3;
+        from->points[5] = +3;
+        from->points[7] = +3;
+        from->points[9] = +2;
+        from->points[11] = -2;
+        from->points[12] = +2;
+        from->points[13] = -1;
+        from->points[16] = -1;
+        from->points[18] = -5;
+        from->points[19] = -2;
+        from->points[20] = -2;
+        from->points[21] = -2;
+
+        to = gibbon_position_copy (gibbon_match_get_current_position (match));
+        to->turn = GIBBON_POSITION_SIDE_BLACK;
+        to->dice[0] = to->dice[1] = 0;
+        to->points[12] = 0;
+        to->points[9] = +1;
+        to->points[7] = +4;
+        to->points[4] = +4;
+        to->points[2] = +3;
+
+        if (!gibbon_match_get_missing_actions (match, to, &result))
+                return FALSE;
+
+        gibbon_position_free (to);
+        g_object_unref (match);
+
+        return TRUE;
+}
+
 int
 main (int argc, char *argv[])
 {
         g_type_init ();
 
         if (!test_bug01 ())
+                return -1;
+        if (!test_bug02 ())
                 return -1;
 
         return 0;
